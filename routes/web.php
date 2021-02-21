74bc904abce502b2a5d821aaabc8aee5f8401fec
mhs-pg <ymanshou@gmail.com>
2021-02-21 21:42:42 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+Route::get('/', function () {
+    return view('user/welcome');
+});
+
+// ユーザー
+Route::namespace('User')->name('user.')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'welcome' => true,
+        'confirm'  => false,
+        'reset'    => false
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:user')->group(function () {
+        // Route::get('/', 'HomeController@welcome');
+        // Homeページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+    });
+});
+
+// 管理者
+Route::namespace('Shop')->prefix('shop')->name('shop.')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'confirm'  => false,
+        'reset'    => false
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:shop')->group(function () {
+
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+
+    });
+
+});
